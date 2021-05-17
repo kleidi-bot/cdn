c85dbbdc620451af9c3152f7e82f9269b9b525d8
Dylan <34420038+DPM97@users.noreply.github.com>
2021-05-17 10:24:07 -0400

    Add files via upload

--- checkout.js
+++ checkout.js
@@ -1,75 +1,29 @@
 /* eslint-disable */
 let cardNumber;
-let stripe = Stripe(publishable_key);
+const stripe = Stripe(publishable_key);
+
 (() => {
-  var elements = stripe.elements({
-    fonts: [
-      {
-        cssSrc: 'https://fonts.googleapis.com/css?family=Quicksand'
-      }
-    ]
-  });
-
-  const elementStyles = {
-    base: {
-      color: '#fff',
-      fontWeight: 600,
-      fontFamily: 'Quicksand, Open Sans, Segoe UI, sans-serif',
-      fontSize: '18px',
-      fontSmoothing: 'antialiased',
-      ':focus': {
-        color: '#fff'
-      },
-      '::placeholder': {
-        color: '#9BACC8'
-      },
-      ':focus::placeholder': {
-        color: '#CFD7DF'
-      }
-    },
-    invalid: {
-      color: '#fff',
-      ':focus': {
-        color: '#FA755A'
-      },
-      '::placeholder': {
-        color: '#FFCCA5'
-      }
-    }
-  };
-
-  const elementClasses = {
-    focus: 'focus',
-    empty: 'empty',
-    invalid: 'invalid'
-  };
-
-  cardNumber = elements.create('cardNumber', {
-    style: elementStyles,
-    classes: elementClasses,
-    placeholder: ''
-  });
-  cardNumber.mount('#cardNumber');
-
-  const cardExpiry = elements.create('cardExpiry', {
-    style: elementStyles,
-    classes: elementClasses,
-    placeholder: ''
-  });
-  cardExpiry.mount('#exp');
-
-  const cardCvc = elements.create('cardCvc', {
-    style: elementStyles,
-    classes: elementClasses,
-    placeholder: ''
-  });
-  cardCvc.mount('#cvc');
-
-  const submit = document.getElementById('submit-button');
-  submit.addEventListener('click', convertCard);
-
+  document
+    .getElementById("submit-button")
+    .addEventListener("click", convertCard);
   window.loaded = true;
 })();
+
+const changePurchaseText = () => {
+  if (!user) {
+    $(".purchase-btn").text("Log In");
+    $(".purchase-btn").removeAttr("type");
+    $(".purchase-btn").attr("id", "submit-login-button");
+    $(".purchase-btn").addClass("login-btn");
+    $(".purchase-btn").attr("onclick", "location.href='/auth/discord'");
+  } else if ($(".bundle.selected").attr("free") === "true") {
+    $(".purchase-btn").text("Join");
+  } else if (subscribed === "true") {
+    $(".purchase-btn").text("Upgrade");
+  } else {
+    $(".purchase-btn").text("Purchase");
+  }
+};
 
 const sendRequest = async (endpoint, body, retries, timeout) => {
   for (let i = 0; i < retries; i++) {
@@ -90,20 +44,20 @@ const sendRequest = async (endpoint, body, retries, timeout) => {
     })();
     console.log(`Attempting ${endpoint}: Try #${i}...`);
   }
-  throw new Error('Request to backend timed out... Please try again later.');
+  throw new Error("Request to backend timed out... Please try again later.");
 };
 
 const setupCard = async (card) => {
   let secret, customer;
 
   const setupResponse = await sendRequest(
-    '/checkout/enforce-payments',
+    "/checkout/enforce-payments",
     {
-      bundle: document.getElementById('bundle').value,
-      fullName: document.getElementById('fullName').value,
-      email: document.getElementById('email').value,
-      coupon: document.getElementById('coupon').value,
-      country: document.getElementById('country').value
+      bundle: document.getElementById("bundle").value,
+      fullName: document.getElementById("fullName").value,
+      email: document.getElementById("email").value,
+      coupon: document.getElementById("coupon").value,
+      country: document.getElementById("country").value,
     },
     10,
     1000
@@ -118,7 +72,7 @@ const setupCard = async (card) => {
   if (setupResponse.data.free) {
     return {
       customer,
-      free: true
+      free: true,
     };
   }
 
@@ -126,10 +80,10 @@ const setupCard = async (card) => {
     payment_method: {
       card,
       billing_details: {
-        name: document.getElementById('fullName').value,
-        email: document.getElementById('email').value
-      }
-    }
+        name: document.getElementById("fullName").value,
+        email: document.getElementById("email").value,
+      },
+    },
   });
 
   if (result.error) throw new Error(result.error.message);
@@ -138,7 +92,7 @@ const setupCard = async (card) => {
     needsIntent: setupResponse.data.needsIntent,
     result,
     customer,
-    free: false
+    free: false,
   };
 };
 
@@ -146,12 +100,12 @@ const intent = async (si) => {
   let cpi = null;
 
   const intentResponse = await sendRequest(
-    '/checkout/generate-intents',
+    "/checkout/generate-intents",
     {
       customer: si.customer,
       payment_method: si.result.setupIntent.payment_method,
       needsIntent: si.needsIntent,
-      coupon: document.getElementById('coupon').value
+      coupon: document.getElementById("coupon").value,
     },
     10,
     1000
@@ -162,7 +116,7 @@ const intent = async (si) => {
 
   if (intentResponse.data.cpi) {
     cpi = await stripe.confirmCardPayment(intentResponse.data.cpi, {
-      payment_method: si.result.setupIntent.payment_method
+      payment_method: si.result.setupIntent.payment_method,
     });
     if (cpi.error) throw new Error(cpi.error.message);
   }
@@ -171,48 +125,48 @@ const intent = async (si) => {
 };
 
 async function convertCard(e) {
-  const submitButton = document.getElementById('submit-button');
+  const submitButton = document.getElementById("submit-button");
   try {
     e.preventDefault();
     submitButton.disabled = true;
-    $('#submit-button').addClass('btn-in-progress');
+    $("#submit-button").addClass("btn-in-progress");
 
     const si = await setupCard(cardNumber); // { result, customer }
     const cpi = si.free ? null : await intent(si);
 
     console.log({
       customer: si.customer,
-      cpi: cpi ? cpi.paymentIntent.id : null
+      cpi: cpi ? cpi.paymentIntent.id : null,
     });
 
     await sendRequest(
-      '/stripe/checkout',
+      "/stripe/checkout",
       {
-        ...$('#checkout-form')
+        ...$("#checkout-form")
           .serializeArray()
           .reduce(function (a, x) {
             a[x.name] = x.value;
             return a;
           }, {}),
         customer: si.customer,
-        cpi: cpi ? cpi.paymentIntent.id : null
+        cpi: cpi ? cpi.paymentIntent.id : null,
       },
       10,
       1000
     );
 
-    window.location.replace('/dashboard/activate');
+    window.location.replace("/dashboard/activate");
   } catch (e) {
     submitButton.disabled = false;
     changePurchaseText();
-    $('#submit-button').removeClass('btn-in-progress');
+    $("#submit-button").removeClass("btn-in-progress");
     window.FlashMessage.error(e.message, {
       progress: true,
       interactive: true,
       timeout: 6000,
       appear_delay: 200,
-      container: '.flash-container',
-      theme: 'dark'
+      container: ".flash-container",
+      theme: "dark",
     });
   }
 }
